eb26ead124b881d21ea816198f01247634d8e0e9
Kai Gellien <kai.gellien@web.de>
2015-03-28 23:38:46 +0200

    started to move functionality from PeriodPlan to TimePlan

--- src/main/scala/de/gellien/timeplanner/timeplan/PeriodPlan.scala
+++ src/main/scala/de/gellien/timeplanner/timeplan/PeriodPlan.scala
@@ -4,46 +4,37 @@ import scala.collection.mutable.ListBuffer
 import scala.collection.immutable.Range
 import TimeHelper._
 
-abstract sealed class PeriodPlan(val withOverview: Boolean) {
+abstract sealed class PeriodPlan(val period: SinglePeriod, val withOverview: Boolean) {
   val header: (String, String, String)
-  val period: SinglePeriod
   def periodOverview: List[SinglePeriod] = PeriodSplitter.splitPeriod(period)
   val periodSpecifics: List[SinglePeriod]
 }
 
-case class WeekPlan(year: Int, week: Int, workList: List[String], override val withOverview: Boolean)(implicit val daysPerWeek: Int) extends PeriodPlan(withOverview) {
+case class WeekPlan(year: Int, week: Int, workList: List[String], override val period: SinglePeriod, override val withOverview: Boolean)(implicit val daysPerWeek: Int) extends PeriodPlan(period, withOverview) {
   override val header = {
     val (start, end) = TimeHelper.fromToWeek(year, week)
     ("%d" format year, "%s -- %s" format (start, end), "W%02d" format week)
   }
-  val todo = ToDoHelper.getTodoByWeek(workList, year, week, removeHeaderPrefix = false)
-  override val period = Week(year, week, todo)
   override val periodSpecifics = for {
     currentDay <- daysInWeek(year, week) take daysPerWeek
     psTodos = ToDoHelper.getTodoByDay(workList, currentDay, removeHeaderPrefix = true)
   } yield Day(currentDay.getYear, currentDay.getMonthOfYear, currentDay.getDayOfMonth, psTodos)
 }
 
-case class MonthPlan(year: Int, month: Int, workList: List[String], override val withOverview: Boolean) extends PeriodPlan(withOverview) {
+case class MonthPlan(year: Int, month: Int, workList: List[String], override val period: SinglePeriod, override val withOverview: Boolean) extends PeriodPlan(period, withOverview) {
   override val header = ("%d" format year, "", "%s" format (TimeHelper.monthName(month)))
-  val todo = ToDoHelper.getTodoByMonth(workList, year, month, removeHeaderPrefix = false)
-  override val period = Month(year, month, todo)
   override val periodSpecifics = for ((weekYear, week) <- weeksInMonth(year, month))
     yield Week(weekYear, week, ToDoHelper.getTodoByWeek(workList, weekYear, week, removeHeaderPrefix = true))
 }
 
-case class QuarterPlan(year: Int, quarter: Int, workList: List[String], override val withOverview: Boolean) extends PeriodPlan(withOverview) {
+case class QuarterPlan(year: Int, quarter: Int, workList: List[String], override val period: SinglePeriod, override val withOverview: Boolean) extends PeriodPlan(period, withOverview) {
   override val header = ("%d" format year, "", "%s" format "Q%d" format quarter)
-  val todo = ToDoHelper.getTodoByQuarter(workList, year, quarter, removeHeaderPrefix = false)
-  override val period = Quarter(year, quarter, todo)
   override val periodSpecifics = for (month <- monthsInQuarter(year, quarter))
     yield Month(year, month, ToDoHelper.getTodoByMonth(workList, year, month, removeHeaderPrefix = true))
 }
 
-case class YearPlan(year: Int, workList: List[String], override val withOverview: Boolean) extends PeriodPlan(withOverview) {
+case class YearPlan(year: Int, workList: List[String], override val period: SinglePeriod, override val withOverview: Boolean) extends PeriodPlan(period, withOverview) {
   override val header = ("%d" format year, "", "")
-  val todo = ToDoHelper.getTodoByYear(workList, year, removeHeaderPrefix = false)
-  override val period = Year(year, todo)
   override val periodSpecifics = for (quarter <- (1 to 4).toList)
     yield Quarter(year, quarter, ToDoHelper.getTodoByQuarter(workList, year, quarter, removeHeaderPrefix = true))
 }
--- src/main/scala/de/gellien/timeplanner/timeplan/TimePlan.scala
+++ src/main/scala/de/gellien/timeplanner/timeplan/TimePlan.scala
@@ -6,28 +6,36 @@ abstract sealed class TimePlan() {
 
 case class WeekTimePlan(year: Int, week: Int)(implicit val daysPerWeek: Int) extends TimePlan {
   override def createPeriodPlan(workList: List[String], withOverview: Boolean): PeriodPlan = {
-    WeekPlan(year, week, workList, withOverview)
+    val todo = ToDoHelper.getTodoByWeek(workList, year, week, removeHeaderPrefix = false)
+    val period = Week(year, week, todo)
+    WeekPlan(year, week, workList, period, withOverview)
   }
   override def toString = "WeekTimePlan(%d, %d)" format (year, week)
 }
 
 case class MonthTimePlan(year: Int, month: Int) extends TimePlan {
   override def createPeriodPlan(workList: List[String], withOverview: Boolean): PeriodPlan = {
-    MonthPlan(year, month, workList, withOverview)
+    val todo = ToDoHelper.getTodoByMonth(workList, year, month, removeHeaderPrefix = false)
+    val period = Month(year, month, todo)
+    MonthPlan(year, month, workList, period, withOverview)
   }
   override def toString = "MonthTimePlan(%d, %d)" format (year, month)
 }
 
 case class QuarterTimePlan(year: Int, quarter: Int) extends TimePlan {
   override def createPeriodPlan(workList: List[String], withOverview: Boolean): PeriodPlan = {
-    QuarterPlan(year, quarter, workList, withOverview)
+  val todo = ToDoHelper.getTodoByQuarter(workList, year, quarter, removeHeaderPrefix = false)
+   val period = Quarter(year, quarter, todo)
+    QuarterPlan(year, quarter, workList, period, withOverview)
   }
   override def toString = "QuarterTimePlan(%d, %d)" format (year, quarter)
 }
 
 case class YearTimePlan(year: Int) extends TimePlan {
   override def createPeriodPlan(workList: List[String], withOverview: Boolean): PeriodPlan = {
-    YearPlan(year, workList, withOverview)
+    val todo = ToDoHelper.getTodoByYear(workList, year, removeHeaderPrefix = false)
+    val period = Year(year, todo)
+    YearPlan(year, workList, period, withOverview)
   }
   override def toString = "YearTimePlan(%d)" format (year)
 }
