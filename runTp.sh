8ec4a6a0eacc6fc966a8dc26295c0db7d7e34f2c
Kai Gellien <kai.gellien@web.de>
2021-01-01 19:19:20 +0100

    temporarily commented out pdflatex calls to ease debugging

--- runTp.sh
+++ runTp.sh
@@ -18,8 +18,8 @@ TODOS="anniversaries.txt appointments2012.txt"
 OUTPUT="--weekschedule24output $WEEKSCHEDULE24.tex --weekscheduleoutput $WEEKSCHEDULE.tex --weekworkplanoutput $WEEKWORKPLAN.tex --dayplanoutput $DAYPLAN.tex --timeplanoutput $TIMEPLAN.tex"
 
 sbt "run $LATEX $TP_OPTS -d -i $TIMEPLAN.txt $OUTPUT $TODOS"
-pdflatex $TIMEPLAN.tex
-pdflatex $DAYPLAN.tex
-pdflatex $WEEKWORKPLAN.tex
-pdflatex $WEEKSCHEDULE.tex
-pdflatex $WEEKSCHEDULE24.tex
+#pdflatex $TIMEPLAN.tex
+#pdflatex $DAYPLAN.tex
+#pdflatex $WEEKWORKPLAN.tex
+#pdflatex $WEEKSCHEDULE.tex
+#pdflatex $WEEKSCHEDULE24.tex
